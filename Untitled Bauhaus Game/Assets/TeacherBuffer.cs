f010fb946fa577379e367bd0b2968e8cd78e5f94
WolfSchaude <wolf.schaude@stud.hochschule-heidelberg.de>
2019-11-26 21:04:28 +0100

    Deleted a testing Variable

--- Untitled Bauhaus Game/Assets/TeacherBuffer.cs
+++ Untitled Bauhaus Game/Assets/TeacherBuffer.cs
@@ -6,12 +6,8 @@ public class TeacherBuffer : MonoBehaviour
 {
 	public static List<Teacher> TeacherBufferList;
 
-	bool once;
-
     void Start()
     {
-		once = true;
-
 		TeacherBufferList = new List<Teacher>();
 
 		TeacherBufferList.Add(new Teacher("Lyonel Feininger",	Resources.Load("Personen/Person1") as Sprite, Teacher.Status.NotHired, Teacher.Beruf.Kunstmaler, true));
@@ -22,14 +18,9 @@ public class TeacherBuffer : MonoBehaviour
 
     void Update()
     {
-		if (once)
-		{
-			TeacherBufferList.Add(new Teacher("Lyonel Feininger", Resources.Load("Personen/Person1") as Sprite, Teacher.Status.NotHired, Teacher.Beruf.Kunstmaler, true));
-			TeacherBufferList.Add(new Teacher("Josef Albers", Resources.Load("Personen/Person2") as Sprite, Teacher.Status.NotHired, Teacher.Beruf.Kunstmaler, true));
-			TeacherBufferList.Add(new Teacher("Johannes Itten", Resources.Load("Personen/Person3") as Sprite, Teacher.Status.NotHired, Teacher.Beruf.Kunstmaler, true));
-
-			once = false;
-		}
+		TeacherBufferList.Add(new Teacher("Lyonel Feininger", Resources.Load("Personen/Person1") as Sprite, Teacher.Status.NotHired, Teacher.Beruf.Kunstmaler, true));
+		TeacherBufferList.Add(new Teacher("Josef Albers", Resources.Load("Personen/Person2") as Sprite, Teacher.Status.NotHired, Teacher.Beruf.Kunstmaler, true));
+		TeacherBufferList.Add(new Teacher("Johannes Itten", Resources.Load("Personen/Person3") as Sprite, Teacher.Status.NotHired, Teacher.Beruf.Kunstmaler, true));
     }
 
 	public Teacher GetTeacher(int index)
