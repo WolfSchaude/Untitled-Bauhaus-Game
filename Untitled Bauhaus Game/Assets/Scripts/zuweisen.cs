84e5410b816bb37ce8ff82cc634d8962af79cf2b
olivermoench <51910141+olivermoench@users.noreply.github.com>
2019-11-27 21:54:42 +0100

    cyxcyxc

--- Untitled Bauhaus Game/Assets/Scripts/zuweisen.cs
+++ Untitled Bauhaus Game/Assets/Scripts/zuweisen.cs
@@ -6,30 +6,18 @@ using UnityEngine.UI;
 public class zuweisen : MonoBehaviour
 {
 
-    public bool showBewerbungzugewiesen;
-
     // Start is called before the first frame update
     void Start()
     {
-        this.GetComponent<Canvas>().enabled = false;
-        //GameObject.Find("bild_eingestellter1").GetComponent<Image>().enabled = false;
-        //GameObject.Find("eingestellter1_text").GetComponent<Text>().enabled = false;
-        //GameObject.Find("bild_eingestellter2").GetComponent<Image>().enabled = false;
-        //GameObject.Find("eingestellter2_text").GetComponent<Text>().enabled = false;
-        //GameObject.Find("bild_eingestellter3").GetComponent<Image>().enabled = false;
-        //GameObject.Find("eingestellter3_text").GetComponent<Text>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("l") && !GameObject.Find("PersonalEinstellen - Canvas").GetComponent<Canvas>().enabled)
+
+        if (GameObject.Find("eingestellter1").activeSelf == true)
         {
-            showBewerbungzugewiesen = !showBewerbungzugewiesen;
-            if (showBewerbungzugewiesen)
-                this.GetComponent<Canvas>().enabled = true;
-            else if (!showBewerbungzugewiesen)
-                this.GetComponent<Canvas>().enabled = false;
+            GameObject.Find("eingestellter1").GetComponent<Button>().onClick.AddListener(() => { GameObject.Find("eingestellter1").GetComponent<Renderer>().material.color = Color.black; });
         }
     }
 }
