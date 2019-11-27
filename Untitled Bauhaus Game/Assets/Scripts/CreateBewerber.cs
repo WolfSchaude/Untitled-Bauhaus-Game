c74027ad2cb095030f781a162583699daa63e88c
olivermoench <51910141+olivermoench@users.noreply.github.com>
2019-11-27 19:53:58 +0100

    Racism is no more

--- Untitled Bauhaus Game/Assets/Scripts/CreateBewerber.cs
+++ Untitled Bauhaus Game/Assets/Scripts/CreateBewerber.cs
@@ -17,26 +17,26 @@ public class CreateBewerber : MonoBehaviour
         //for (int i = 0; i < TeacherBuffer.TeacherBufferList.Count; i++)
         {
             GameObject bewerbung1 = Instantiate(bewerbung, GameObject.Find("ContentEinstellen").transform);
+            GameObject eingestellter1 = Instantiate(bewerbung, parent.transform);
+            eingestellter1.SetActive(false);
             bewerbung1.GetComponentInChildren<Text>().text = "Name: " + TeacherLoader.tb.Buffer[0].Name + Environment.NewLine + "Beruf: " + TeacherLoader.tb.Buffer[0].TeacherBeruf;
             bewerbung1.GetComponent<Image>().sprite = TeacherLoader.tb.Buffer[0].Picture;
-            
+            bewerbung1.GetComponent<Button>().onClick.AddListener(() => { eingestellter1.SetActive(true); eingestellter1.GetComponent<Image>().sprite = TeacherLoader.tb.Buffer[0].Picture; eingestellter1.GetComponentInChildren<Text>().text = "Name: " + TeacherLoader.tb.Buffer[0].Name + Environment.NewLine + "Beruf: " + TeacherLoader.tb.Buffer[0].TeacherBeruf; bewerbung1.SetActive(false); });
+
             GameObject bewerbung2 = Instantiate(bewerbung, GameObject.Find("ContentEinstellen").transform);
+            GameObject eingestellter2 = Instantiate(bewerbung, parent.transform);
+            eingestellter2.SetActive(false);
             bewerbung2.GetComponentInChildren<Text>().text = "Name: " + TeacherLoader.tb.Buffer[1].Name + Environment.NewLine + "Beruf: " + TeacherLoader.tb.Buffer[1].TeacherBeruf;
             bewerbung2.GetComponent<Image>().sprite = TeacherLoader.tb.Buffer[1].Picture;
+            bewerbung2.GetComponent<Button>().onClick.AddListener(() => { eingestellter2.SetActive(true); eingestellter2.GetComponent<Image>().sprite = TeacherLoader.tb.Buffer[1].Picture; eingestellter2.GetComponentInChildren<Text>().text = "Name: " + TeacherLoader.tb.Buffer[1].Name + Environment.NewLine + "Beruf: " + TeacherLoader.tb.Buffer[1].TeacherBeruf; bewerbung2.SetActive(false); });
 
             GameObject bewerbung3 = Instantiate(bewerbung, GameObject.Find("ContentEinstellen").transform);
+            GameObject eingestellter3 = Instantiate(bewerbung, parent.transform);
+            eingestellter3.SetActive(false);
             bewerbung3.GetComponentInChildren<Text>().text = "Name: " + TeacherLoader.tb.Buffer[2].Name + Environment.NewLine + "Beruf: " + TeacherLoader.tb.Buffer[2].TeacherBeruf;
             bewerbung3.GetComponent<Image>().sprite = TeacherLoader.tb.Buffer[2].Picture;
-            
-            CreateEingestellter();
+            bewerbung3.GetComponent<Button>().onClick.AddListener(() => { eingestellter3.SetActive(true); eingestellter3.GetComponent<Image>().sprite = TeacherLoader.tb.Buffer[2].Picture; eingestellter3.GetComponentInChildren<Text>().text = "Name: " + TeacherLoader.tb.Buffer[2].Name + Environment.NewLine + "Beruf: " + TeacherLoader.tb.Buffer[2].TeacherBeruf; bewerbung3.SetActive(false); });
         }
-    }
-
-    void CreateEingestellter()
-    {
-        GameObject eingestellter1 = Instantiate(bewerbung, parent.transform);
-        GameObject eingestellter2 = Instantiate(bewerbung, parent.transform);
-        GameObject eingestellter3 = Instantiate(bewerbung, parent.transform);
     }
 
     // Update is called once per frame
