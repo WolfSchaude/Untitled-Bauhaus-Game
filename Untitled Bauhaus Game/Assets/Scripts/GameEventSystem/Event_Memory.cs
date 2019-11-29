2730aeebaef06392e8d66f8042ab2c4c99e60e43
WolfSchaude <wolf.schaude@stud.hochschule-heidelberg.de>
2019-11-29 15:43:50 +0100

    Interface einheitlich auf größere Größe

---
+++ Untitled Bauhaus Game/Assets/Scripts/GameEventSystem/Event_Memory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_Memory : MonoBehaviour
+{
+	public Event Memory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Memory = new Event();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public Event_Memory(Event ev)
+	{
+		Memory = ev;
+	}
+
+	public void SetMemory(Event ev)
+	{
+		Memory = ev;
+	}
+}
