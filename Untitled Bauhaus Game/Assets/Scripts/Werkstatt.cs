61fd2d2f49facdad07607f1192048bbb6994b558
RascalFoxfire <11011223@stud.hochschule-heidelberg.de>
2019-11-29 14:29:43 +0100

    Building System
    Verschiedene Qualitäten und einfaches Erfahrungssystem implementiert

--- Untitled Bauhaus Game/Assets/Scripts/Werkstatt.cs
+++ Untitled Bauhaus Game/Assets/Scripts/Werkstatt.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Werkstatt : MonoBehaviour
 {
+    public enum WerkstattTyp { Malerei, Architektur, Ausstellungsgestaltung, Metallwerkstatt, Tischlerei };
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Werkstatt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
