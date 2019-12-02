d859238491a709d33a0c632f7f8115a159f0cbc9
Recorsi <47929124+Recorsi@users.noreply.github.com>
2019-12-02 14:28:50 +0100

    Zuweisen Switch fix

--- Untitled Bauhaus Game/Assets/Scripts/ZuweisenOnChange.cs
+++ Untitled Bauhaus Game/Assets/Scripts/ZuweisenOnChange.cs
@@ -5,24 +5,23 @@ using UnityEngine;
 
 public class ZuweisenOnChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+	void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
-	void OnChange()
+	void HandleInputData(int val)
 	{
-		switch (Convert.ToInt32(this.GetComponent<Dropdown>()))
+		switch (val)
 		{
 			case 0:
-				Debug.Log("nicht zugewiesen");
+				Debug.Log("Nicht zugewiesen");
 				break;
 			case 1:
 				Debug.Log("Ist der Architekturwerkstatt zugewiesen");
@@ -50,4 +49,41 @@ public class ZuweisenOnChange : MonoBehaviour
 				break;
 		}
 	}
+
+
+	//void OnChange()
+	//{
+	//	switch (Convert.ToInt32(this.GetComponent<Dropdown>()))
+	//	{
+	//		case 0:
+	//			Debug.Log("nicht zugewiesen");
+	//			break;
+	//		case 1:
+	//			Debug.Log("Ist der Architekturwerkstatt zugewiesen");
+	//			break;
+	//		case 2:
+	//			Debug.Log("Ist der Malerei zugewiesen");
+	//			break;
+	//		case 3:
+	//			Debug.Log("Ist der Ausstellungsgestaltung zugewiesen");
+	//			break;
+	//		case 4:
+	//			Debug.Log("Ist der Metallwerkstatt zugewiesen");
+	//			break;
+	//		case 5:
+	//			Debug.Log("Ist der Tischlerei zugewiesen");
+	//			break;
+	//		case 6:
+	//			Debug.Log("Ist dem Lehrsaal zugewiesen");
+	//			break;
+	//		case 7:
+	//			Debug.Log("Ist dem Wohnheim zugewiesen");
+	//			break;
+	//		default:
+	//			Debug.Log("Nicht zugewiesen");
+	//			break;
+	//	}
+	//}
+
+
 }
