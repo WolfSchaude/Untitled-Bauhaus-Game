13ba5e86e48f3fc2af978d9e7de831d03fd173d4
olivermoench <51910141+olivermoench@users.noreply.github.com>
2019-12-03 16:16:40 +0100

    kjkjk

---
+++ Untitled Bauhaus Game/Assets/Scripts/WinLose.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLose : MonoBehaviour
+{
+
+	public Politikmeter politikmeterSkript;
+    // Start is called before the first frame update
+    void Start()
+    {
+		politikmeterSkript = GameObject.Find("Politikmeter").GetComponent<Politikmeter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(politikmeterSkript.Politiklevel >= 200)
+		{
+			Application.Quit();
+			Debug.Log("Bruh exit");
+		}
+		if(politikmeterSkript.Politiklevel <= 0)
+		{
+			Application.Quit();
+			Debug.Log("also bruh exit");
+		}
+    }
+}
