408985feb28a67fc92d9d4c6e974171c91589c9d
RascalFoxfire <11011223@stud.hochschule-heidelberg.de>
2019-12-03 15:48:31 +0100

    Building System
    Bug fixes

--- Untitled Bauhaus Game/Assets/Scripts/Buildingsystem/BaueWerkstatt.cs
+++ Untitled Bauhaus Game/Assets/Scripts/Buildingsystem/BaueWerkstatt.cs
@@ -22,7 +22,15 @@ public class BaueWerkstatt : MonoBehaviour
     public GameObject werk6;
     public GameObject werk7;
 
-    public bool[] WerkstattTyp;
+    public Werkstatt Script1;
+    public Werkstatt Script2;
+    public Werkstatt Script3;
+    public Werkstatt Script4;
+    public Werkstatt Script5;
+    public Werkstatt Script6;
+    public Werkstatt Script7;
+
+    public int WerkstattTyp;
 
     public bool buildInProgress = false;
 
@@ -30,11 +38,7 @@ public class BaueWerkstatt : MonoBehaviour
     {
         MaxQualitaet = 1.5f;
         MinQualität = 0.5f + (AnzahlWerkstaette * 0.05f);
-        WerkstattTyp = new bool[5];
-        for (int i = 0; i <= 4; i++)
-        {
-            WerkstattTyp[i] = false;
-        }
+        WerkstattTyp = 0;
     }
 
     void Update()
@@ -56,29 +60,39 @@ public class BaueWerkstatt : MonoBehaviour
         {
             if (werk6.activeSelf)
             {
+                Debug.Log("Lol1");
                 werk7.SetActive(true);
+                werk7.GetComponent<Werkstatt>().SetType(WerkstattTyp);
             }
             if (werk5.activeSelf)
             {
+                Debug.Log("LOL2");
                 werk6.SetActive(true);
+                werk6.GetComponent<Werkstatt>().SetType(WerkstattTyp);
             }
             if (werk4.activeSelf)
             {
                 werk5.SetActive(true);
+                werk5.GetComponent<Werkstatt>().SetType(WerkstattTyp);
             }
             if (werk3.activeSelf)
             {
                 werk4.SetActive(true);
+                werk4.GetComponent<Werkstatt>().SetType(WerkstattTyp);
             }
             if (werk2.activeSelf)
             {
                 werk3.SetActive(true);
+                werk3.GetComponent<Werkstatt>().SetType(WerkstattTyp);
             }
             if (werk1.activeSelf)
             {
                 werk2.SetActive(true);
+                Script2.DebugWST = WerkstattTyp;
             }
             werk1.SetActive(true);
+            Script1.DebugWST = WerkstattTyp;
+            Debug.Log(Script1.DebugWST);
 
             if (werk2.activeSelf) //wird erst ab werk2 ausgeführt
             {
@@ -95,7 +109,7 @@ public class BaueWerkstatt : MonoBehaviour
 
     public void SetType()
     {
-        WerkstattTyp[GameObject.Find("EventSystem").GetComponent<BaumenuDetail>().buttonCount] = true;
+        WerkstattTyp = GameObject.Find("EventSystem").GetComponent<BaumenuDetail>().buttonCount;
     }
 
     public void NeueWerkstatt()
