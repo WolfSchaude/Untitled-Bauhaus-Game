ddf7de81d163ea8d67de3d0902367f2bf4e5a689
Calvin <11011151@stud.hochschule-heidelberg.de>
2019-12-03 16:15:10 +0100

    build, new scene and scene change

---
+++ Untitled Bauhaus Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    public void Quit()
+    {
+        
+            Application.Quit();
+
+        
+
+    }
+}
