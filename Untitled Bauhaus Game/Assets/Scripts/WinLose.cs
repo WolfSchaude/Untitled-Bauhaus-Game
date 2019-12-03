ed64c2a8c1ffb0ec1ba04f3e287ab91400052798
olivermoench <51910141+olivermoench@users.noreply.github.com>
2019-12-03 17:03:48 +0100

    Lose Bedingung

--- Untitled Bauhaus Game/Assets/Scripts/WinLose.cs
+++ Untitled Bauhaus Game/Assets/Scripts/WinLose.cs
@@ -6,11 +6,33 @@ public class WinLose : MonoBehaviour
 {
 
 	public Politikmeter politikmeterSkript;
-    // Start is called before the first frame update
-    void Start()
+	public Money moneySkript;
+	public int tage;
+
+	public int StartMonth;
+	public int StartDay;
+	public int StartHour;
+	public int StartMinute;
+	public int StartSecond;
+
+	private float gameTime;
+	private const float MinToSec = 60;
+	private const float HourToSec = 60 * 60;
+	private const float DayToSec = 60 * 60 * 24;
+	private const float MonthToSec = 60 * 60 * 24 * 31;
+
+	// Start is called before the first frame update
+	void Start()
     {
 		politikmeterSkript = GameObject.Find("Politikmeter").GetComponent<Politikmeter>();
-    }
+		moneySkript = GameObject.Find("Money Display").GetComponent<Money>();
+
+		gameTime += StartSecond;
+		gameTime += StartMinute * MinToSec;
+		gameTime += StartHour * HourToSec;
+		gameTime += StartDay * DayToSec;
+		gameTime += StartMonth * MonthToSec;
+	}
 
     // Update is called once per frame
     void Update()
@@ -25,5 +47,10 @@ public class WinLose : MonoBehaviour
 			Application.Quit();
 			Debug.Log("also bruh exit");
 		}
+		if(moneySkript.money < 0)
+		{
+			//Bruh muss ich noch machen
+		}
+
     }
 }
