05b51115f33851f4f156db43b8784ccf5a7cb74b
Recorsi <47929124+Recorsi@users.noreply.github.com>
2019-12-05 14:22:07 +0100

    Gebäude anklicken mit Highlighting

---
+++ Untitled Bauhaus Game/Assets/Scripts/ChangeMatOnClick.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMatOnClick : MonoBehaviour
+{
+	public Material StandardMat;
+	public Material OnClickMat;
+
+	private bool outlineShown = false;
+	private bool mouseOver = false;
+
+	void Start()
+    {
+		//Fetch the Material from the Renderer of the GameObject
+		//StandardMat = GetComponent<Renderer>().material;
+	}
+
+    void Update()
+    {
+		if (Input.GetMouseButtonDown(0) && outlineShown && !mouseOver)
+		{
+			GetComponent<MeshRenderer>().material = StandardMat;
+			outlineShown = false;
+		}
+	}
+
+	void OnMouseOver()
+	{
+		mouseOver = true;
+		if (Input.GetMouseButtonDown(0) || outlineShown)
+		{
+			GetComponent<MeshRenderer>().material = OnClickMat;
+			outlineShown = true;
+		}
+	}
+
+	void OnMouseExit()
+	{
+		mouseOver = false;
+	}
+}
