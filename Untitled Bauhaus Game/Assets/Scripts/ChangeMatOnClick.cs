99c7492b866bf0173fc57a922e6323a7730841b4
Recorsi <47929124+Recorsi@users.noreply.github.com>
2019-12-06 17:52:36 +0100

    Merge branch 'Bausystem-Variante-2'

---
+++ Untitled Bauhaus Game/Assets/Scripts/ChangeMatOnClick.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMatOnClick : MonoBehaviour
+{
+	public Material StandardMat;
+	public Material OnClickMat;
+
+	public bool isSelected = false;
+	private bool mouseOver = false;
+
+	void Start()
+    {
+
+	}
+
+    void Update()
+    {
+
+	}
+
+	public void closeWindow()
+	{
+		string[] menuname = {"TestCube1", "TestCube2", "TestCube3", "TestCube4", "TestCube5"};
+		for (int i = 0; i < menuname.Length; i++)
+		{
+			GameObject.Find(menuname[i]).GetComponent<ChangeMatOnClick>().isSelected = false;
+			GameObject.Find(menuname[i]).GetComponent<MeshRenderer>().material = StandardMat;
+		}
+		
+	}
+	void OnMouseOver() //Checks if the mouse is over the object
+	{
+		mouseOver = true;
+		if (Input.GetMouseButtonDown(0) && !GameObject.Find("TestCube1").GetComponent<ChangeMatOnClick>().isSelected && !GameObject.Find("TestCube2").GetComponent<ChangeMatOnClick>().isSelected && !GameObject.Find("TestCube3").GetComponent<ChangeMatOnClick>().isSelected && !GameObject.Find("TestCube4").GetComponent<ChangeMatOnClick>().isSelected && !GameObject.Find("TestCube5").GetComponent<ChangeMatOnClick>().isSelected)
+		{
+			GetComponent<MeshRenderer>().material = OnClickMat;
+			isSelected = true;
+		}
+	}
+
+	void OnMouseExit() //Checks if the mouse exits the object
+	{
+		mouseOver = false;
+	}
+}
---
+++ Untitled Bauhaus Game/Assets/Scripts/NewMenu.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewMenu : MonoBehaviour
+{
+	public GameObject MenuWindow;
+	public GameObject MenuPanel;
+	public GameObject Prefab;
+
+	GameObject[] dropdownArr;
+
+	public Text Name;
+
+	public int oldValue = 0;
+	Vector3 menuPos;
+
+	public bool IsClicked;
+
+	public int activeChecker = 0;
+
+	void Start()
+	{
+		IsClicked = false;
+
+		dropdownArr = new GameObject[5];
+		dropdownArr[0] = (GameObject)Instantiate(Prefab, MenuWindow.transform);
+		dropdownArr[1] = (GameObject)Instantiate(Prefab, MenuWindow.transform);
+		dropdownArr[2] = (GameObject)Instantiate(Prefab, MenuWindow.transform);
+		dropdownArr[3] = (GameObject)Instantiate(Prefab, MenuWindow.transform);
+		dropdownArr[4] = (GameObject)Instantiate(Prefab, MenuWindow.transform);
+	}
+
+	void Update()
+	{
+		checkActive();
+		setMenu();
+		activate();
+
+		//Set UI to GameObject position
+		MenuWindow.transform.position = menuPos;
+
+		if (!IsClicked)
+		{
+			//MenuWindow.transform.position = Input.mousePosition + new Vector3(-400, -300, 0); //set window to mouse position
+			IsClicked = true;
+		}
+
+		if (!MenuWindow.activeSelf)
+		{
+			IsClicked = false;
+		}
+	}
+
+	public void activate()
+	{
+		if (GameObject.Find("TestCube1").GetComponent<ChangeMatOnClick>().isSelected || GameObject.Find("TestCube2").GetComponent<ChangeMatOnClick>().isSelected || GameObject.Find("TestCube3").GetComponent<ChangeMatOnClick>().isSelected || GameObject.Find("TestCube4").GetComponent<ChangeMatOnClick>().isSelected || GameObject.Find("TestCube5").GetComponent<ChangeMatOnClick>().isSelected)
+		{
+			MenuWindow.SetActive(true);
+		}
+		else
+		{
+			MenuWindow.SetActive(false);
+		}
+	}
+
+	public void checkActive()
+	{
+		string[] test = { "TestCube1", "TestCube2", "TestCube3", "TestCube4", "TestCube5" };
+		for (int i = 0; i < test.Length; i++)
+		{
+			if (GameObject.Find(test[i]).GetComponent<ChangeMatOnClick>().isSelected)
+			{
+				activeChecker = i + 1;
+				menuPos = Camera.main.WorldToScreenPoint(GameObject.Find(test[i]).transform.position); //Update Position
+				break;
+			}
+		}
+	}
+	public void setMenu()
+	{
+		switch (activeChecker)
+		{
+			case 1:
+				//MenuDropdown.options.Add(new Dropdown.OptionData() { text = "test" });
+				//for (int x = 0; x < MenuDropdown.options.Count; x++)
+				//{
+				//	if (MenuDropdown.options[x].text == "test")
+				//	{
+				//		MenuDropdown.options.RemoveAt(x);
+				//		//break;
+				//	}
+				//}
+				Name.text = "Werkstatt 1";
+				dropdownArr[0].SetActive(true);
+				dropdownArr[1].SetActive(false);
+				dropdownArr[2].SetActive(false);
+				dropdownArr[3].SetActive(false);
+				dropdownArr[4].SetActive(false);
+				break;
+			case 2:
+				Name.text = "Werkstatt 2";
+				dropdownArr[0].SetActive(false);
+				dropdownArr[1].SetActive(true);
+				dropdownArr[2].SetActive(false);
+				dropdownArr[3].SetActive(false);
+				dropdownArr[4].SetActive(false);
+				break;
+			case 3:
+				Name.text = "Werkstatt 3";
+				dropdownArr[0].SetActive(false);
+				dropdownArr[1].SetActive(false);
+				dropdownArr[2].SetActive(true);
+				dropdownArr[3].SetActive(false);
+				dropdownArr[4].SetActive(false);
+				break;
+			case 4:
+				Name.text = "Werkstatt 4";
+				dropdownArr[0].SetActive(false);
+				dropdownArr[1].SetActive(false);
+				dropdownArr[2].SetActive(false);
+				dropdownArr[3].SetActive(true);
+				dropdownArr[4].SetActive(false);
+				break;
+			case 5:
+				Name.text = "Werkstatt 5";
+				dropdownArr[0].SetActive(false);
+				dropdownArr[1].SetActive(false);
+				dropdownArr[2].SetActive(false);
+				dropdownArr[3].SetActive(false);
+				dropdownArr[4].SetActive(true);
+				break;
+		}
+	}
+}
