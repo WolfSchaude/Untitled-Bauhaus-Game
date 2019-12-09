cef9e187546f14749ce8f910869050868256356a
Recorsi <47929124+Recorsi@users.noreply.github.com>
2019-12-09 14:23:23 +0100

    Fenster für Inventar

---
+++ Untitled Bauhaus Game/Assets/Scripts/ExpoInventory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpoInventory : MonoBehaviour
+{
+	public GameObject inventoryWindow;
+
+	void Start()
+    {
+		inventoryWindow.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+	public void showWindow() //Sets detail window active
+	{
+		if (!inventoryWindow.activeSelf)
+		{
+			inventoryWindow.SetActive(true);
+			GameObject.Find("EventSystem").GetComponent<bewerbungvisible>().bewerbungGameObject.SetActive(false);
+			GameObject.Find("EventSystem").GetComponent<bewerbungvisible>().zuweisenGameObject.SetActive(false);
+			GameObject.Find("EventSystem").GetComponent<BaumenuDetail>().detailWindow.SetActive(false);
+		}
+		else
+		{
+			inventoryWindow.SetActive(false);
+		}
+	}
+}
