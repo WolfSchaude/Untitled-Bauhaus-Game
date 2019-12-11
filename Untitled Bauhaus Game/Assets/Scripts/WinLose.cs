6e78c4b1691a8e5216836a3f028041dc4dfea612
Recorsi <47929124+Recorsi@users.noreply.github.com>
2019-12-11 11:32:26 +0100

    Game Over Screen

--- Untitled Bauhaus Game/Assets/Scripts/WinLose.cs
+++ Untitled Bauhaus Game/Assets/Scripts/WinLose.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UntitledBauhausGame;
 
 public class WinLose : MonoBehaviour
 {
@@ -10,39 +12,59 @@ public class WinLose : MonoBehaviour
 	public Money moneySkript;
 	public Studenten studentenSkript;
 
+	public GameObject GameOverScreen;
+
+	public Text GameOverText;
+
 	private int PleiteThreshold = 30;
 	private int PleiteCounter = 0;
 
-	// Start is called before the first frame update
 	void Start()
     {
 		politikmeterSkript = GameObject.Find("Politikmeter").GetComponent<Politikmeter>();
 		moneySkript = GameObject.Find("Money Display").GetComponent<Money>();
 		studentenSkript = GameObject.Find("Studenten Counter").GetComponent<Studenten>();
+		GameOverScreen.SetActive(false);
 	}
 
-    // Update is called once per frame
     void Update()
     {
+		if (GameOverScreen.activeSelf)
+		{
+			Time.timeScale = 0;
+			Kamera.AbleToMove = false;
+		}
+		else
+		{
+			Time.timeScale = FastForward.oldTimeScale;
+		}
         if(politikmeterSkript.Politiklevel >= 200)
 		{
-			SceneManager.LoadScene("Main_Menu");
-			Debug.Log("Du bist zu weit rechts");
+			//SceneManager.LoadScene("Main_Menu");
+			//Debug.Log("Du bist zu weit rechts");
+			GameOverScreen.SetActive(true);
+			GameOverText.text = "Die Hochschule ist zu weit rechts";
 		}
 		if(politikmeterSkript.Politiklevel <= 0)
 		{
-			SceneManager.LoadScene("Main_Menu");
-			Debug.Log("Du bist zu weit links");
+		//	SceneManager.LoadScene("Main_Menu");
+			//Debug.Log("Du bist zu weit links");
+			GameOverScreen.SetActive(true);
+			GameOverText.text = "Die Hochschule ist zu weit links";
 		}
 		if(PleiteCounter >= PleiteThreshold)
 		{
-			SceneManager.LoadScene("Main_Menu");
-			Debug.Log("Sorry, bist pleite gegangen");
+			//SceneManager.LoadScene("Main_Menu");
+			//Debug.Log("Sorry, bist pleite gegangen");
+			GameOverScreen.SetActive(true);
+			GameOverText.text = "Die Hochschule ist pleite gegangen";
 		}
 		if(studentenSkript.StudentenAnzahl <= 0)
 		{
-			SceneManager.LoadScene("Main_Menu");
-			Debug.Log("Du hast keine Studenten mehr");
+			//SceneManager.LoadScene("Main_Menu");
+			//Debug.Log("Du hast keine Studenten mehr");
+			GameOverScreen.SetActive(true);
+			GameOverText.text = "Die Hochschule hat keine Studenten mehr.";
 		}
     }
 
