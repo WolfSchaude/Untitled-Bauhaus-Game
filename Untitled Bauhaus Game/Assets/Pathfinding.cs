3a573ef6c0a661e126c4b405f99bf42efeb0df63
olivermoench <51910141+olivermoench@users.noreply.github.com>
2019-12-13 11:01:09 +0100

    Pathfinding für den Ansehen Würfel🎅

---
+++ Untitled Bauhaus Game/Assets/Pathfinding.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathfinding : MonoBehaviour
+{
+	public Transform[] points;
+	private NavMeshAgent nav;
+	private int destPoint;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		nav = GetComponent<NavMeshAgent>();
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		if (!nav.pathPending && nav.remainingDistance < 0.5f)
+			GoToNextPoint();
+	}
+	void GoToNextPoint()
+	{
+		if (points.Length == 0)
+		{
+			return;
+		}
+		nav.destination = points[destPoint].position;
+		destPoint = (destPoint + 1) % points.Length;
+	}
+}
