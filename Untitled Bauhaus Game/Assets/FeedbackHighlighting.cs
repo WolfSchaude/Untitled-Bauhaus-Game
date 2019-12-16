d3299d0385abb31eb4d5a77d1e507b5720a1e232
WolfSchaude <wolf.schaude@stud.hochschule-heidelberg.de>
2019-12-16 11:23:21 +0100

    New Feedback Cards are highlighted. Can be de-highlighted by clicking or after two weeks

---
+++ Untitled Bauhaus Game/Assets/FeedbackHighlighting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FeedbackHighlighting : MonoBehaviour
+{
+    int x = 0;
+    void Start()
+    {
+        GameObject.Find("EventSystem").GetComponent<DatumRelatedEvents>().changedDay.AddListener(() => Increment());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (x >= 14)
+        {
+            DisableRedBorder();
+        }
+    }
+
+    public void DisableRedBorder()
+    {
+        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(380, 60);
+    }
+
+    void Increment()
+    {
+        x++;
+    }
+}
