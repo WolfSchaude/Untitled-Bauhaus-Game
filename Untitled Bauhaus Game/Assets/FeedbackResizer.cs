697af4e8d5cb14f77d67dde6389d4ff143e2bdd4
WolfSchaude <wolf.schaude@stud.hochschule-heidelberg.de>
2019-12-17 16:43:39 +0100

    Resizer Start

---
+++ Untitled Bauhaus Game/Assets/FeedbackResizer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackResizer : MonoBehaviour
+{
+    public GameObject Vorlage;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(gameObject.GetComponent<RectTransform>().sizeDelta.x, Vorlage.GetComponent<RectTransform>().sizeDelta.y + 10);
+    }
+}
