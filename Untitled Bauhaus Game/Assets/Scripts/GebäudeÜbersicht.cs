bd3a4f6fab756a6828f93f5a42a760cc3604c773
WolfSchaude <wolf.schaude@stud.hochschule-heidelberg.de>
2019-12-18 10:48:44 +0100

    New Helper Sidebar Menu, shows some general Getting Started Stuff

--- Untitled Bauhaus Game/Assets/Scripts/GebäudeÜbersicht.cs
+++ Untitled Bauhaus Game/Assets/Scripts/GebäudeÜbersicht.cs
@@ -22,9 +22,9 @@ public class GebäudeÜbersicht : MonoBehaviour
     {
         OverviewAnimator.SetBool("Bool", Collapsed);
 
-        werkstattCount.text = "Gebaute Werkstätte: \n" + GameObject.Find("UI").GetComponent<BaueArchitekturwerkstatt>().AnzahlWerkstaette.ToString() + " / 7"; 
-        lehrsaalCount.text = "Gebaute Lehrsäle: \n" + GameObject.Find("UI").GetComponent<BaueLehrsaal>().AnzahlLehrsaal.ToString() + " / 8"; 
-        wohnheimCount.text = "Gebaute Wohnheime: \n" + GameObject.Find("UI").GetComponent<BaueWohnheim>().AnzahlWohnheime.ToString() + " / 3"; 
+        if (werkstattCount != null) werkstattCount.text = "Gebaute Werkstätte: \n" + GameObject.Find("UI").GetComponent<BaueArchitekturwerkstatt>().AnzahlWerkstaette.ToString() + " / 7";
+        if (lehrsaalCount != null) lehrsaalCount.text = "Gebaute Lehrsäle: \n" + GameObject.Find("UI").GetComponent<BaueLehrsaal>().AnzahlLehrsaal.ToString() + " / 8";
+        if ( wohnheimCount != null) wohnheimCount.text = "Gebaute Wohnheime: \n" + GameObject.Find("UI").GetComponent<BaueWohnheim>().AnzahlWohnheime.ToString() + " / 3"; 
     }
 
     public void ToggleOpened() //Sets overview window active
