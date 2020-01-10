23d437969b900d1ee445a9fb9c9ac96084b73797
RascalFoxfire <11011223@stud.hochschule-heidelberg.de>
2020-01-10 14:47:02 +0100

    Building System
    Framework für neues Bausystem

---
+++ Untitled Bauhaus Game/Assets/Scripts/Buildingsystem/Bausystem.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bausystem : MonoBehaviour
+{
+    public NewTimeKeeper TimeHandler;
+
+    private List<GameObject> Structures;
+
+    public Vector3[,,,] StructureData;
+
+    private bool SomethingIsBuilded;
+
+    public int NumberOfStyles;
+    public int NumberOfBuildingTypes;
+    public int NumberOfBuildingsPerType;
+    public int TypeToBuild;
+
+    private int DaysSinceStartedBuilding;
+
+    private int[] BuildedTypes;
+
+
+    void Start()
+    {
+        List<GameObject> Structures = new List<GameObject>();
+
+        Structures.Add(GameObject.Find("Abteil_1"));
+        Structures.Add(GameObject.Find("Abteil_2"));
+        Structures.Add(GameObject.Find("Abteil_3"));
+        Structures.Add(GameObject.Find("Abteil_4"));
+        Structures.Add(GameObject.Find("Abteil_5"));
+        Structures.Add(GameObject.Find("Abteil_6"));
+        Structures.Add(GameObject.Find("Abteil_7"));
+        Structures.Add(GameObject.Find("Abteil_9"));
+        Structures.Add(GameObject.Find("Abteil_10"));
+        Structures.Add(GameObject.Find("Abteil_11"));
+        Structures.Add(GameObject.Find("Abteil_12"));
+        Structures.Add(GameObject.Find("Abteil_13"));
+        Structures.Add(GameObject.Find("Abteil_14"));
+        Structures.Add(GameObject.Find("Abteil_15"));
+        Structures.Add(GameObject.Find("Abteil_16"));
+        Structures.Add(GameObject.Find("Abteil_17"));
+        Structures.Add(GameObject.Find("Abteil_18"));
+
+        NumberOfStyles = 2;
+        NumberOfBuildingTypes = 3;
+        NumberOfBuildingsPerType = 7;
+
+        Vector3[,,,] StructureData = new Vector3[NumberOfStyles, NumberOfBuildingTypes, NumberOfBuildingsPerType, 2];
+
+        DaysSinceStartedBuilding = 0;
+
+        TypeToBuild = 0;
+        SomethingIsBuilded = false;
+    }
+
+
+    void Update()
+    {
+        BuildTime();
+    }
+
+    private void BuildTime()
+    {
+        if (DaysSinceStartedBuilding < 60)
+        {
+            
+        }
+    }
+
+    public void StartBuilding()
+    {
+        if (SomethingIsBuilded == true)
+        {
+
+        }
+    }
+
+    public void BuildStructure()
+    {
+        TypeToBuild = 0;
+        SomethingIsBuilded = false;
+    }
+}
---
+++ Untitled Bauhaus Game/Assets/Scripts/Buildingsystem/Struktur.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Struktur : MonoBehaviour
+{
+    public enum Type { Undefiniert, Architektur, Malerei, Ausstellungsgestaltung, Metallwerkstatt, Tischlerei };
+
+    public Type OwnTypeEnum { get; private set; }
+
+    public int OwnTypeInt { get; private set; }
+
+    void Start()
+    {
+        OwnTypeInt = 0;
+        OwnTypeEnum = Type.Undefiniert;
+    }
+
+
+}
