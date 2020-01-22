1081ca39852620a26baa2cd9fca4e7b8e0f7a3b0
WolfSchaude <wolf.schaude@stud.hochschule-heidelberg.de>
2020-01-22 15:07:17 +0100

    Camer lock on hover over Feedback/Events

---
+++ Untitled Bauhaus Game/Assets/DisableCamOnMouseOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DisableCamOnMouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Kamera.LockCameraMovement.Invoke();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Kamera.FreeCameraMovement.Invoke();
+    }
+}
