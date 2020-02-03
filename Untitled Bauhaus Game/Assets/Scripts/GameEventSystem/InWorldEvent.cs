5e365736cba95ffc8e1ef4006fb63ffd617c46f6
WolfSchaude <wolf.schaude@stud.hochschule-heidelberg.de>
2020-02-03 09:47:01 +0100

    Event PopUp added

--- Untitled Bauhaus Game/Assets/Scripts/GameEventSystem/InWorldEvent.cs
+++ Untitled Bauhaus Game/Assets/Scripts/GameEventSystem/InWorldEvent.cs
@@ -23,21 +23,17 @@ public class InWorldEvent : MonoBehaviour
 
 	#region References to the UI Elements, which shall be updated
 	/// <summary>
-	/// The title Text field of the window
-	/// </summary>
-	[SerializeField] Text _EventTitle;
-	/// <summary>
 	/// The description Text field of the window
 	/// </summary>
 	[SerializeField] Text _EventDescription;
 	/// <summary>
 	/// The Button which selects option 1
 	/// </summary>
-	[SerializeField] Button _EventOption1;
+	[SerializeField] Text _EventOption1Description;
 	/// <summary>
 	/// The button which selects option 2
 	/// </summary>
-	[SerializeField] Button _EventOption2;
+	[SerializeField] Text _EventOption2Description;
 
 	#endregion
 	private void Awake()
@@ -51,16 +47,15 @@ public class InWorldEvent : MonoBehaviour
 
 	void SetContent(Event eventValues)
 	{
-		_EventTitle.text = "ID: " + eventValues.ID.ToString();
 		_EventDescription.text = eventValues.EventText;
 
-		_EventOption1.GetComponentInChildren<Text>().text
+		_EventOption1Description.text
 			= eventValues.EventOption1 + Environment.NewLine
 			+ "Ansehensveränderung: " + eventValues.Option1_Ansehen + Environment.NewLine
 			+ "Politische Tragweite: " + eventValues.Option1_Politik + Environment.NewLine
 			+ "Kosten: " + eventValues.Option1_Geld * -1 + " RM";
 
-		_EventOption2.GetComponentInChildren<Text>().text
+		_EventOption2Description.text
 			= eventValues.EventOption2 + Environment.NewLine
 			+ "Ansehensveränderung: " + eventValues.Option2_Ansehen + Environment.NewLine
 			+ "Politische Tragweite: " + eventValues.Option2_Politik + Environment.NewLine
